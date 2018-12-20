a9a1614197788c88ed8532023378649894d92ae8
Max Likhachev <max142343245@gmail.com>
2018-12-20 19:39:31 +0300

    try to draw cards

---
+++ SetGame/BorderButton.swift
@@ -0,0 +1,12 @@
+//
+//  Button.swift
+//  SetGame
+//
+//  Created by Максим Лихачев on 20/12/2018.
+//  Copyright © 2018 Максим Лихачев. All rights reserved.
+//
+
+import Foundation
+@IBDesignable class SetCardButton: BorderButton {
+
+}
---
+++ SetGame/CardButton.swift
@@ -0,0 +1,9 @@
+//
+//  CardButton.swift
+//  SetGame
+//
+//  Created by Максим Лихачев on 20/12/2018.
+//  Copyright © 2018 Максим Лихачев. All rights reserved.
+//
+
+import Foundation
